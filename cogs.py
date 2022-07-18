5bb105b1fcb5017c36f07e8376aa29723b2d4db6
Scitinz <43889072+Scitinz@users.noreply.github.com>
2022-07-18 13:07:51 +0100

    Added shutdown command

--- cogs.py
+++ cogs.py
@@ -16,6 +16,13 @@ class ChoreBotCog(commands.Cog):
         self.lastbincollect = None
         self.binsout = False
 
+    #Shutdown command
+    @commands.command()
+    async def stop(self, ctx):
+        print("Shutting down...")
+        await self.bot.close()
+        exit()
+
     @commands.command()
     async def hello(self, ctx):
         print("Hello command used")
